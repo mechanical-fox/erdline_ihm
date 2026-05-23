a9703a89d2b3a358a063f18714cfacdafe00feb3
Pierre Meunier <pierre.meunier.etu@univ-lille.fr>
2026-05-23 17:37:25 +0200

    :bulb: update comment 2

--- src/script/util/SessionTrackerUtil.ts
+++ src/script/util/SessionTrackerUtil.ts
@@ -17,7 +17,7 @@ export class SessionTrackerUtil{
     }
 
     /** Call the API, to change the backgrounds saved in database by the backgrounds actually present in the graphic interface.
-     * This function has no effect if the function, if the function startTracking(sessionId), wasn't called before.
+     * This function has no effect if the function startTracking(sessionId), wasn't called before.
      * If called very often, this function will wait a little before calling the API. To prevent calling the API, 5 time by second.*/
     static async notifyBackgroundChange(){
         
@@ -58,7 +58,7 @@ export class SessionTrackerUtil{
     }
 
     /** Call the API, to change the characters saved in database by the characters actually present in the graphic interface.
-     * This function has no effect if the function, if the function startTracking(sessionId), wasn't called before.
+     * This function has no effect if the function startTracking(sessionId), wasn't called before.
      * If called very often, this function will wait a little before calling the API. To prevent calling the API, 5 time by second.*/
     static async notifyCharacterChange(){
         
